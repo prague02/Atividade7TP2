778406cb1af3850ea6408b954e2cc3b692788498
Laura <laura.contact02@gmail.com>
2025-04-30 17:54:35 -0300

    Create AppConfig.java

---
+++ AppConfig.java
@@ -0,0 +1,14 @@
+package fatec.edu.br.demo.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+
+@Configuration
+public class AppConfig {
+
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
+}
